f6142ea4ea7468e5609c96d1e2d925e78eb6831a
Mikayla Solomon <mikaylasolomon68@gmail.com>
2025-08-14 18:55:55 -0400

    fully functional website

--- pages/urls.py
+++ pages/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from pages import views
 
 urlpatterns = [
-    path("", views.home, name='home')
-]
+    path("", views.home, name='home'),
+    path("<int:pk>", views.detail, name='detail') #pass along the number = primary key for each blog post in the url
+    ]
